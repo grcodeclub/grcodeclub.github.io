b347ca361c56892fbd6ea196198da9bf7dd6dfe1
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-23 22:00:02 +0300

    Update menu.js

--- orologia/menu.js
+++ orologia/menu.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
     var  menuLink = '<li><a href="https://grcodeclub.github.io">Αρχική Σελίδα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    var  menuLink = '<li><u">Ανά Κατηγορία</u></li>';
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/orologia/">Ορολογία σχετικά με τον Προγραμματισμό</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/programming/orologia/">Προγραμματισμός</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/network/orologia/">Δίκτυα Υπολογιστών</a></li>';
+    menuLink = '<li><a href="https://grcodeclub.github.io/network/orologia/">Ορολογία σχετικά με τα Δίκτυα Υπολογιστών</a></li>';
     $('.sidebarMenuInner').append(menuLink);
      menuLink = '<li><a href="https://grcodeclub.github.io/linux/orologia/">Linux</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/system/orologia/">Λειτουργικό Σύστημα</a></li>';
+    menuLink = '<li><a href="https://grcodeclub.github.io/system/orologia/">Ορολογία σχετικά με το Λειτουργικό Σύστημα</a></li>';
     $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.github.io/cybersecurity/orologia/">Κυβερνοασφάλεια</a></li>';
+    menuLink = '<li><a href="https://grcodeclub.github.io/cybersecurity/orologia/">Ορολογία σχετικά με την Κυβερνοασφάλεια</a></li>';
     $('.sidebarMenuInner').append(menuLink);
     menuLink = '<li style="height: 200px;"></li>';
     $('.sidebarMenuInner').append(menuLink);
