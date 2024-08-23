13f917501e0ef71bba54670828be002fa6417a71
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-23 22:19:13 +0300

    Create syntomografies.js

---
+++ orologia/menu/syntomografies.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    var  menuLink = '<li><a href="https://grcodeclub.github.io">Αρχική Σελίδα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="https://grcodeclub.github.io/programming/orologia/syntomografies">Συντομογραφίες σχετικά με τον Προγραμματισμό</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="https://grcodeclub.github.io/network/orologia/syntomografies">Συντομογραφίες σχετικά με τα Δίκτυα Υπολογιστών</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+     menuLink = '<li><a href="https://grcodeclub.github.io/linux/orologia/syntomografies">Συντομογραφίες σχετικά με το Linux</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="https://grcodeclub.github.io/system/orologia/syntomografies">Συντομογραφίες σχετικά με το Λειτουργικό Σύστημα</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li><a href="https://grcodeclub.github.io/cybersecurity/orologia/syntomografies">Συντομογραφίες σχετικά με την Κυβερνοασφάλεια</a></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    menuLink = '<li style="height: 200px;"></li>';
+    $('.sidebarMenuInner').append(menuLink);
+    
+  });
+  
