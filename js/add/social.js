a95b6fe765ee21a00ec5098a75e0e1f5c70b6312
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-25 02:16:20 +0300

    Update social.js

--- js/add/social.js
+++ js/add/social.js
@@ -8,7 +8,7 @@ if (addFooter) {
     <li class="social-item"> <a class="facebook" href="https://www.facebook.com/grcode.club/" target="_blank"><span></span> <span></span> <span></span> <span></span><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
     <li class="social-item"> <a class="instagram" href="https://www.instagram.com/grcode.club" target="_blank"><span></span> <span></span> <span></span> <span></span><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
     <li class="social-item"> <a class="github" href="https://github.com/GrCodeClub" target="_blank"><span></span> <span></span> <span></span> <span></span><i class="fa fa-github" aria-hidden="true"></i></a></li>
-    <li class="social-item"> <a class="github" href="#" onclick="copyRealUrl(); return false;"><span></span> <span></span> <span></span> <span></span>&#128279;</a></li>
-  </ul>`;
+    <li class="social-item"> <a class="github" href="#" onclick="copyRealUrl(); return false;" ><span></span> <span></span> <span></span> <span></span><i class="fa fa-share-alt" aria-hidden="true"></i></a></li>
+ </ul>`;
     addFooter.innerHTML = htmlContent;
 }
