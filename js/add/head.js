a6dbee242c86ff74feeaed4661b174352f0ecf57
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-24 23:39:05 +0300

    Update head.js

--- js/add/head.js
+++ js/add/head.js
@@ -129,6 +129,8 @@ var var_url = document.createElement('script');
 var_url.src = 'https://grcodeclub.gr/js/url.js';
 document.head.appendChild(var_url);
 
-var var_share = document.createElement('script');
-var_share.src = 'https://grcodeclub.gr/js/add/share.js';
-document.body.appendChild(var_share);
+document.addEventListener('DOMContentLoaded', function() {
+    var var_share = document.createElement('script');
+    var_share.src = 'https://grcodeclub.gr/js/add/share.js';
+    document.body.appendChild(var_share);
+});
