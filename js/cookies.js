b4bedfedc33aa1183ac7cd49e94c018e6ab152a4
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-24 16:40:36 +0300

    Create cookies.js

---
+++ js/cookies.js
@@ -0,0 +1,75 @@
+document.addEventListener('DOMContentLoaded', function() {
+    if (!getCookie('cookiesAccepted')) {
+        document.getElementById('cookie-banner').style.display = 'block';
+    }
+
+    var acceptCookiesButton = document.getElementById('accept-cookies');
+    var noacceptCookiesButton = document.getElementById('reject-cookies');
+    
+    let cookieStatus = getCookie('cookiesAccepted');
+
+    if (cookieStatus === 'false') {
+        document.getElementById('cookie-banner').style.display = 'block';
+    }
+    if (noacceptCookiesButton) {
+            noacceptCookiesButton.onclick = function() {
+            setCookie('cookiesAccepted', 'false', 1);
+            document.getElementById('cookie-banner').style.display = 'none';
+        };
+    
+    }
+    if (acceptCookiesButton) {
+        acceptCookiesButton.onclick = function() {
+            setCookie('cookiesAccepted', 'true', 365);
+            document.getElementById('cookie-banner').style.display = 'none';
+            var cookieSwitch = document.getElementById('cookies-ga4');
+                if (cookieSwitch.checked) {
+                    loadTrackingScripts();
+                }
+        };
+    } else {
+        console.error('Accept Cookies button not found.');
+    }
+});
+
+
+
+
+
+
+  function setCookie(name, value, days) {
+    let date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + (value || "") + ";" + expires + ";path=/";
+  }
+
+  function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+
+  function loadTrackingScripts() {
+    var gtmScript = document.createElement('script');
+    gtmScript.async = true;
+    gtmScript.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-MMLRVK48';
+    document.head.appendChild(gtmScript);
+
+    var gaScript = document.createElement('script');
+    gaScript.async = true;
+    gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-KP7YGYPW0R';
+    document.head.appendChild(gaScript);
+
+    gaScript.onload = function() {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-KP7YGYPW0R');
+    };
+  }
