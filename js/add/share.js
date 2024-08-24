83e48ae6b31e1b91c08c1ea33209e822b4dfb636
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-25 00:26:20 +0300

    Delete js/add/share.js

--- js/add/share.js
+++
@@ -1,5 +0,0 @@
-const addFooter = document.getElementsByClassName('Text');
-const htmlContent = `<button onclick="copyRealUrl()">Share</button>`;
-
-// Προσθέτουμε το νέο περιεχόμενο στην αρχή του υπάρχοντος
-addFooter.innerHTML = htmlContent + addFooter.innerHTML;
