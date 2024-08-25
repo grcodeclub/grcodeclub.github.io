07468feff33ca141ecce0bb119dae1e52dbd9d4e
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-25 15:18:07 +0300

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -109,7 +109,7 @@ function loadMetaPixel() {
             if(f.fbq) return; n=f.fbq=function() {
                 n.callMethod ? n.callMethod.apply(n,arguments) : n.queue.push(arguments);
             };
-            if(!f._fbq) f._fbq=n; n.push=n; n.loaded=!0; n.version='2.0';
+            if(!f._fbq) f._fbq=n; n.push=n; n.loaded=!0; n.version='2.9.165';
             n.queue=[]; t=b.createElement(e); t.async=!0;
             t.src=v; s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s);
