daa0b32f7f03acc1add5d51410ce7ead01325d48
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-25 23:55:09 +0300

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function() {
         document.getElementById('cookie-banner').style.display = 'block';
     }
     if (getCookie('cookiesAccepted')) {
-       loadTrackingScripts();
+       event();
 }
         
     var acceptCookiesButton = document.getElementById('accept-cookies');
@@ -78,32 +78,21 @@ function setCookie_minutes(name, value, minutes) {
     return null;
   }
 
-function loadTrackingScripts() {
-    // Ελέγχουμε αν το script του Google Analytics έχει ήδη φορτωθεί
-    if (!window.gtagInitialized) {
-        var gaScript = document.createElement('script');
-        gaScript.async = true;
-        gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-KP7YGYPW0R';
-        document.head.appendChild(gaScript);
+ function loadTrackingScripts() {
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MMLRVK48');
 
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-
-        gtag('js', new Date());
-        gtag('config', 'G-KP7YGYPW0R', {
-            'cookie_update': false  // Αποτρέπει την ανανέωση του cookie σε κάθε φόρτωση της σελίδας
-        });
-
-        // Σημειώνουμε ότι το script έχει αρχικοποιηθεί
-        window.gtagInitialized = true;
-    } else {
-        // Αν το script είναι ήδη φορτωμένο, μόνο καταχωρούμε το event
-        gtag('config', 'G-KP7YGYPW0R', {
-            'cookie_update': false
-        });
-    }
 }
 
+function event(){
+    window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-KP7YGYPW0R'); 
+}
 
 function loadMetaPixel() {
    !function(f,b,e,v,n,t,s){
