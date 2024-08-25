65bf1974e5f8aca4b3e31d4fb72bb317e251a489
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-25 15:47:09 +0300

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -98,28 +98,20 @@ function setCookie_minutes(name, value, minutes) {
   }
 
 function loadMetaPixel() {
-    var script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://connect.facebook.net/en_US/fbevents.js';
-
-    script.onload = function() {
-            fbq('init', '1233204201195274');
-            fbq('track', 'PageView');
-    };
-
-    document.head.appendChild(script);
-
-    script.onerror = function() {
-        console.error('Failed to load fbevents.js');
-    };
-
-    var noscript = document.createElement('noscript');
-    var img = document.createElement('img');
-    img.height = 1;
-    img.width = 1;
-    img.style.display = 'none';
-    img.src = 'https://www.facebook.com/tr?id=1233204201195274&ev=PageView&noscript=1';
-    noscript.appendChild(img);
-
-    document.body.appendChild(noscript);
+   !function(f,b,e,v,n,t,s){
+       if(f.fbq)return;
+       n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+       if(!f._fbq)f._fbq=n;
+       n.push=n;
+       n.loaded=!0;
+       n.version='2.0';
+       n.queue=[];
+       t=b.createElement(e);
+       t.async=!0;
+       t.src=v;
+       s=b.getElementsByTagName(e)[0];
+       s.parentNode.insertBefore(t,s)}
+    (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1233204201195274');
+    fbq('track', 'PageView');
 }
