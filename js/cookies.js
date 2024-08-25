880bf7cc90f2efb5b22e2354e547564c7ae3f0ca
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-25 22:51:36 +0300

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -4,13 +4,13 @@ window.addEventListener('load', function() {
     }
 
     if (getCookie('cookiesAccepted')) {
-  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MMLRVK48');
-    }
-    
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-KP7YGYPW0R');
+}
+        
+   
 
     var acceptCookiesButton = document.getElementById('accept-cookies');
     var noacceptCookiesButton = document.getElementById('reject-cookies');
