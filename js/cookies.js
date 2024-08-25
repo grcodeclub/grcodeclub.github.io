24373feef29988d8d4103ccbf9051048abaf6147
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-25 23:26:02 +0300

    Update cookies.js

--- js/cookies.js
+++ js/cookies.js
@@ -4,7 +4,7 @@ window.addEventListener('load', function() {
     }
 
     if (getCookie('cookiesAccepted')) {
-        loadTagsGoogle();
+        loadTrackingScripts();
     }
         
     var acceptCookiesButton = document.getElementById('accept-cookies');
@@ -79,26 +79,16 @@ function setCookie_minutes(name, value, minutes) {
     return null;
   }
 
-function loadTagsGoogle(){  
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+function loadTrackingScripts() {
+
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MMLRVK48');
-}
-
-function loadTrackingScripts() {
-
-  var gaScript = document.createElement('script');
-  gaScript.async = true;
-  gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-KP7YGYPW0R';
-  document.head.appendChild(gaScript);
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-KP7YGYPW0R'); 
+})(window,document,'script','dataLayer','GTM-MMLRVK48');</script>
+    
   }
+
 function loadMetaPixel() {
    !function(f,b,e,v,n,t,s){
        if(f.fbq)return;
