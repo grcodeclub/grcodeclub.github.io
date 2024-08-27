918e9fa9fc5fb00e35a1742ceb20347f3c5cb1e3
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-27 20:39:25 +0300

    Update basic.js

--- js/menu/basic.js
+++ js/menu/basic.js
@@ -1,15 +1,18 @@
-$(document).ready(function() {
-    var  menuLink = '<li><a href="https://grcodeclub.github.io">Αρχική Σελίδα</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href=https://grcodeclub.gr/programming/"">Προγραμματισμός</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.gr/linux/">Linux</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.gr/windows/">Microsoft Windows</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.gr/cybersecurity/">Κυβερνοασφάλεια</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-    menuLink = '<li><a href="https://grcodeclub.gr/network/">Δίκτυα Υπολογιστών</a></li>';
-    $('.sidebarMenuInner').append(menuLink);
-  });
-  
+window.onload = function() {
+    var menuLinks = [
+        '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
+        '<li><a href="https://grcodeclub.gr/programming/">Προγραμματισμός</a></li>',
+        '<li><a href="https://grcodeclub.gr/linux/">Linux</a></li>',
+        '<li><a href="https://grcodeclub.gr/windows/">Microsoft Windows</a></li>',
+        '<li><a href="https://grcodeclub.gr/cybersecurity/">Κυβερνοασφάλεια</a></li>',
+        '<li><a href="https://grcodeclub.gr/network/">Δίκτυα Υπολογιστών</a></li>',
+        '<li style="height: 200px;"></li>'
+    ];
+    
+    var $sidebarMenuInners = document.querySelectorAll('.sidebarMenuInner');
+    $sidebarMenuInners.forEach(function($sidebarMenuInner) {
+    menuLinks.forEach(function(link) {
+        $sidebarMenuInner.insertAdjacentHTML('beforeend', link);
+    });
+});
+};
