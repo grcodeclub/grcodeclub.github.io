79dc9ac5fe362d8f422f78d1959cfcfb6e290da8
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-27 17:20:09 +0300

    Update back_page.js

--- js/add/back_page.js
+++ js/add/back_page.js
@@ -2,5 +2,5 @@ var h1Element = document.getElementById('back_page');
         
         if (h1Element) {
             // Αν το στοιχείο υπάρχει, ρυθμίζουμε το περιεχόμενο
-            h1Element.innerHTML = '<span><i class="fa fa-arrow-left" aria-hidden="true" style="background-color: rgb(255, 0, 0); padding: 10px; border-radius: 50%;"></i></span>';
+            h1Element.innerHTML = '<span><i class="fa fa-arrow-left" aria-hidden="true" style="background-color: black; padding: 10px; border-radius: 50%;"></i></span>';
         } 
