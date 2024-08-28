bc9ecc040c8539ee2a96d8878052f09deb4cf2ec
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-28 22:35:56 +0300

    Update data.js

--- js/add/data.js
+++ js/add/data.js
@@ -146,6 +146,12 @@ document.addEventListener('DOMContentLoaded', function() {
         menu.src = 'https://grcodeclub.gr/js/menu/system.js';
         document.body.appendChild(menu);
     }
+
+       if (document.getElementById('back_page')) {  
+        var script_back_page = document.createElement('script');
+        script_back_page.src = 'https://grcodeclub.gr/js/add/back_page.js';
+        document.body.appendChild(script_back_page);
+      }
     
     var headder_menu = document.createElement('script');
     headder_menu.src = 'https://grcodeclub.gr/js/add/headder_with_menu.js';
