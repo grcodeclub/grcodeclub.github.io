af2147dd6be94f93cf8a011b13ee1fb58253cdb5
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-28 20:15:17 +0300

    Update network.js

--- js/menu/network.js
+++ js/menu/network.js
@@ -1,33 +1,36 @@
 window.onload = function() {
-    if (document.body.id === 'cloud') {
-        var menuLinks = [
-        '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
-        '<li><a href="https://grcodeclub.gr/cloud/">Cloud</a></li>',
-        '<li><a href="https://grcodeclub.gr/cloud/basic">Βασικές Γνώσεις</a></li>',
-        '<li><a href="https://grcodeclub.gr/network/">Δίκτυα Υπολογιστών</a></li>',
-        '<li style="height: 200px;"></li>'
-        ];    
+    function addMenuLinks(menuLinks) {
         var $sidebarMenuInners = document.querySelectorAll('.sidebarMenuInner');
         $sidebarMenuInners.forEach(function($sidebarMenuInner) {
-        menuLinks.forEach(function(link) {$sidebarMenuInner.insertAdjacentHTML('beforeend', link);});
+            menuLinks.forEach(function(link) {
+                $sidebarMenuInner.insertAdjacentHTML('beforeend', link);
+            });
         });
     }
-    if (document.body.id === 'network') {
-            var menuLinks = [
-        '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
-        '<li><a href="https://grcodeclub.gr/network/">Δίκτυα Υπολογιστών</a></li>',
-        '<li><a href="https://grcodeclub.gr/network/protocol">Πρωτόκολλα</a></li>',
-        '<li><a href="https://grcodeclub.gr/network/more/list/services-port">Υπηρεσίες και Ports</a></li>',
-        '<li><a href="https://grcodeclub.gr/network/more/list/application-and-protocols">Εφαρμογή και Πρωτόκολλο</a></li>',
-        '<li><a href="https://grcodeclub.gr/network/orologia">Ορολογία</a></li>',
-        '<li><a href="https://grcodeclub.gr/network/more">Έγγραφα</a></li>',
-        '<li><a href="https://grcodeclub.gr/network/cloud/">Cloud</a></li>',
-        '<li style="height: 200px;"></li>'
+
+    if (document.body.id === 'cloud') {
+        var cloudMenuLinks = [
+            '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
+            '<li><a href="https://grcodeclub.gr/cloud/">Cloud</a></li>',
+            '<li><a href="https://grcodeclub.gr/cloud/basic">Βασικές Γνώσεις</a></li>',
+            '<li><a href="https://grcodeclub.gr/network/">Δίκτυα Υπολογιστών</a></li>',
+            '<li style="height: 200px;"></li>'
         ];
-        var $sidebarMenuInners = document.querySelectorAll('.sidebarMenuInner');
-        $sidebarMenuInners.forEach(function($sidebarMenuInner) {
-        menuLinks.forEach(function(link) {$sidebarMenuInner.insertAdjacentHTML('beforeend', link);});
-        });
+        addMenuLinks(cloudMenuLinks);
+    }
+
+    if (document.body.id === 'network') {
+        var networkMenuLinks = [
+            '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
+            '<li><a href="https://grcodeclub.gr/network/">Δίκτυα Υπολογιστών</a></li>',
+            '<li><a href="https://grcodeclub.gr/network/protocol">Πρωτόκολλα</a></li>',
+            '<li><a href="https://grcodeclub.gr/network/more/list/services-port">Υπηρεσίες και Ports</a></li>',
+            '<li><a href="https://grcodeclub.gr/network/more/list/application-and-protocols">Εφαρμογή και Πρωτόκολλο</a></li>',
+            '<li><a href="https://grcodeclub.gr/network/orologia">Ορολογία</a></li>',
+            '<li><a href="https://grcodeclub.gr/network/more">Έγγραφα</a></li>',
+            '<li><a href="https://grcodeclub.gr/network/cloud/">Cloud</a></li>',
+            '<li style="height: 200px;"></li>'
+        ];
+        addMenuLinks(networkMenuLinks);
     }
 };
-
