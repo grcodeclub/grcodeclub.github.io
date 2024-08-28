678b59e01ca85fb007bb786e201a1f52515984d7
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-28 17:26:27 +0300

    Update tables-pages.js

--- js/search/tables-pages.js
+++ js/search/tables-pages.js
@@ -4,10 +4,6 @@ let currentPage = 1;
 const table1 = document.getElementById('search_table_page');
 const rows1 = table1.querySelectorAll('tr');
 const headerRow = table1.querySelector('#title-page');
-console.log(headerRow);
-
-console.log("Table");
-console.log(rows1);
 
 function displayTable(page) {
     const start = (page - 1) * itemsPerPage + 1; // +1 to skip header row
