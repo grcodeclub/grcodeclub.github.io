c2994abfeb9fc642a2d1863116bb22e57e176d39
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-28 20:42:29 +0300

    Update system.js

--- js/menu/system.js
+++ js/menu/system.js
@@ -1,4 +1,14 @@
 window.onload = function() {
+    function addMenuLinks(menuLinks) {
+        var $sidebarMenuInners = document.querySelectorAll('.sidebarMenuInner');
+        $sidebarMenuInners.forEach(function($sidebarMenuInner) {
+            menuLinks.forEach(function(link) {
+                $sidebarMenuInner.insertAdjacentHTML('beforeend', link);
+            });
+        });
+    }
+
+    if (document.body.id === 'system') {
     var menuLinks = [
         '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
         '<li><a href="https://grcodeclub.github.io/system">Λειτουργικό Σύστημα</a></li>',
@@ -9,10 +19,17 @@ window.onload = function() {
         '<li style="height: 200px;"></li>'
     ];
     
-    var $sidebarMenuInners = document.querySelectorAll('.sidebarMenuInner');
-    $sidebarMenuInners.forEach(function($sidebarMenuInner) {
-    menuLinks.forEach(function(link) {
-        $sidebarMenuInner.insertAdjacentHTML('beforeend', link);
-    });
-});
+        addMenuLinks(cloudMenuLinks);
+    }
+
+    if (document.body.id === 'android') {
+        var networkMenuLinks = [
+        '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
+        '<li><a href="https://grcodeclub.github.io/system/android">Android</li>',
+        '<li><a href="https://grcodeclub.github.io/system/android/versions">Eκδόσεις</a></li>',
+        '<li><a href="https://grcodeclub.github.io/system">Λειτουργικά Συστήματα</a></li>',
+        '<li style="height: 200px;"></li>'
+        ];
+        addMenuLinks(networkMenuLinks);
+    }
 };
