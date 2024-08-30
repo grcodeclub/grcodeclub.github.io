6bbf14d2d0134fc2730a7a05c139cf9ccf87c929
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-08-30 14:25:20 +0300

    Update headder.js

--- js/add/headder.js
+++ js/add/headder.js
@@ -8,7 +8,7 @@ const htmlContent = `
 `;
 
 // Εύρεση του στόχου με το id 'add_headder'
-const addHeadderDiv = document.getElementById('add_headder_and_menu');
+const addHeadderDiv = document.getElementById('add_headder');
 
 // Προσθήκη του HTML περιεχομένου στο div με το id 'add_headder'
 if (addHeadderDiv) {
