dfbf9339f8129b49abe717777b41725241d562af
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-09-19 09:16:46 -0400

    Update social.js

--- js/add/social.js
+++ js/add/social.js
@@ -5,10 +5,30 @@ const addFooter = document.getElementById('addFooter');
 if (addFooter) {
     // Περιεχόμενο HTML που θα προστεθεί στο footer
     const htmlContent = `<ul class="social">
-    <li class="social-item"> <a class="facebook" href="https://www.facebook.com/grcode.club/" target="_blank"><span></span> <span></span> <span></span> <span></span><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-    <li class="social-item"> <a class="instagram" href="https://www.instagram.com/grcode.club" target="_blank"><span></span> <span></span> <span></span> <span></span><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-    <li class="social-item"> <a class="github" href="https://github.com/GrCodeClub" target="_blank"><span></span> <span></span> <span></span> <span></span><i class="fa fa-github" aria-hidden="true"></i></a></li>
-    <li class="social-item"> <a class="github" href="#" onclick="shareURL(); return false;" ><span></span> <span></span> <span></span> <span></span><i class="fa fa-share" aria-hidden="true"></i></a></li>
+<li class="social-item">
+    <a class="facebook" href="https://www.facebook.com/grcode.club/" target="_blank">
+        <span></span> <span></span> <span></span> <span></span>
+        <i class="fa-brands fa-facebook" aria-hidden="true"></i>
+    </a>
+</li>
+<li class="social-item">
+    <a class="instagram" href="https://www.instagram.com/grcode.club" target="_blank">
+        <span></span> <span></span> <span></span> <span></span>
+        <i class="fa-brands fa-instagram" aria-hidden="true"></i>
+    </a>
+</li>
+<li class="social-item">
+    <a class="github" href="https://github.com/GrCodeClub" target="_blank">
+        <span></span> <span></span> <span></span> <span></span>
+        <i class="fa-brands fa-github" aria-hidden="true"></i>
+    </a>
+</li>
+<li class="social-item">
+    <a class="share" href="#" onclick="shareURL(); return false;">
+        <span></span> <span></span> <span></span> <span></span>
+        <i class="fa-solid fa-share" aria-hidden="true"></i>
+    </a>
+</li>
  </ul>`;
     addFooter.innerHTML = htmlContent;
 }
