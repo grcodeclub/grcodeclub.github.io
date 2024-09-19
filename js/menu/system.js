f34f837a234678a14e1279958317cc3efc93e19a
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-09-19 09:52:13 -0400

    Update system.js

--- js/menu/system.js
+++ js/menu/system.js
@@ -26,9 +26,9 @@ window.onload = function() {
         if (document.body.id === 'linux') {
     var menuLinks = [
         '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
-        '<li><a href="https://grcodeclub.github.io/linux/">Linux</li>',
-        '<li><a href="https://grcodeclub.github.io/linux/terminal">Terminal</li>',
-        '<li><a href="https://grcodeclub.github.io/linux/install">Εγκατάσταση Προγραμμάτων</li>',
+        '<li><a href="https://grcodeclub.github.io/linux"><span class="fa-brands fa-linux" aria-hidden="true"></span> Linux</li>',
+        '<li><a href="https://grcodeclub.github.io/linux/terminal"><span class="fa-solid fa-terminal" aria-hidden="true"></span> Terminal</li>',
+        '<li><a href="https://grcodeclub.github.io/linux/install"><span class="fa-solid fa-terminal" aria-hidden="true"></span> Εγκατάσταση Προγραμμάτων</li>',
         '<li><a href="https://grcodeclub.github.io/linux/files">Αρχεία</a></li>',
         '<li><a href="https://grcodeclub.github.io/linux/orologia">Ορολογία</a></li>',
         '<li><a href="https://grcodeclub.github.io/linux/more/distro">Λίστα με τα Distro</a></li>',
@@ -41,11 +41,11 @@ window.onload = function() {
             if (document.body.id === 'windows') {
     var menuLinks = [
             '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
-        '<li><a href="https://grcodeclub.github.io/windows">Microsoft Windows</a></li>',
+        '<li><a href="https://grcodeclub.github.io/windows"><span class="fa-brands fa-windows" aria-hidden="true"></span> Microsoft Windows</a></li>',
         '<li><a href="https://grcodeclub.github.io/windows/orologia">Ορολογία</a></li>',
-        '<li><a href="https://grcodeclub.github.io/windows/command">Γραμμή εντολών</a></li>',
-        '<li><a href="https://grcodeclub.github.io/windows/command/powershell">PowerShell</a></li>',
-        '<li><a href="https://grcodeclub.github.io/windows/more/run">Run Commands</a></li>',
+        '<li><a href="https://grcodeclub.github.io/windows/command"><span class="fa-solid fa-terminal" aria-hidden="true"></span> Γραμμή εντολών</a></li>',
+        '<li><a href="https://grcodeclub.github.io/windows/command/powershell"><span class="fa-solid fa-terminal" aria-hidden="true"></span> PowerShell</a></li>',
+        '<li><a href="https://grcodeclub.github.io/windows/more/run"><span class="fa-solid fa-terminal" aria-hidden="true"></span> Run Commands</a></li>',
         '<li><a href="https://grcodeclub.github.io/windows/more/language-id">Language ID</a></li>',
         '<li style="height: 200px;"></li>'
     ];
@@ -56,7 +56,7 @@ window.onload = function() {
     if (document.body.id === 'android') {
         var networkMenuLinks = [
         '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
-        '<li><a href="https://grcodeclub.github.io/system/android">Android</li>',
+        '<li><a href="https://grcodeclub.github.io/system/android"><span class="fa-brands fa-android" aria-hidden="true"></span> Android</li>',
         '<li><a href="https://grcodeclub.github.io/system/android/versions">Eκδόσεις</a></li>',
         '<li><a href="https://grcodeclub.github.io/system">Λειτουργικά Συστήματα</a></li>',
         '<li style="height: 200px;"></li>'
