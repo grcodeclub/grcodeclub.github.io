d90a7859161dfe1551d49e85188b8991c660f21c
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-09-29 22:56:26 +0300

    Update main-header.js

--- js/main_page/main-header.js
+++ js/main_page/main-header.js
@@ -3,15 +3,16 @@ window.addEventListener('scroll', function() {
     var logoImg = document.getElementById('logo');
 
     if (window.scrollY > 10) {
-        header.style.backgroundColor = 'transparent';
-        header.style.backdropFilter = 'blur(10px)';
+        header.style.backgroundColor = 'rgba(50, 50, 50, 0.9)'; // Σκούρο γκρι
+        header.style.backdropFilter = 'blur(8px)'; // Θόλωμα
         header.style.opacity = '1';
-
+        
         logoImg.style.display = 'block';
     } else {
-        header.style.backgroundColor = 'transparent';
+        header.style.backgroundColor = 'rgba(50, 50, 50, 0.9)'; // Σκούρο γκρι
         header.style.backdropFilter = 'blur(0px)';
         header.style.opacity = '1';
+        
         logoImg.style.display = 'none';
     }
 });
