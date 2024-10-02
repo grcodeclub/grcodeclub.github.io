4c23232ffb2f4b6a64599107bbbd5cf5fece89ac
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-02 04:44:50 -0400

    Create mobile.js

---
+++ js/menu/mobile.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+    function addMenuLinks(menuLinks) {
+        var $sidebarMenuInners = document.querySelectorAll('.sidebarMenuInner');
+        $sidebarMenuInners.forEach(function($sidebarMenuInner) {
+            menuLinks.forEach(function(link) {
+                $sidebarMenuInner.insertAdjacentHTML('beforeend', link);
+            });
+        });
+    }
+
+    if (document.body.id === 'mobile') {
+        var cloudMenuLinks = [
+            '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
+            '<li><a href="https://grcodeclub.gr/network/mobile/">Mobile</a></li>',
+            '<li><a href="https://grcodeclub.gr/network/mobile/android">Android</a></li>',
+            '<li style="height: 200px;"></li>'
+        ];
+        addMenuLinks(cloudMenuLinks);
+    }
+
+};
