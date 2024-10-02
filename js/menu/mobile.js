33142687a4f697cfca996fe8cd264bf155295c02
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-02 07:22:47 -0400

    Update mobile.js

--- js/menu/mobile.js
+++ js/menu/mobile.js
@@ -12,23 +12,10 @@ window.onload = function() {
         menuLinks = [
             '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
             '<li><a href="https://grcodeclub.gr/network/mobile/">Mobile</a></li>',
-            '<li><a href="https://grcodeclub.gr/mobile/android">Android</a></li>',
+            '<li><a href="https://grcodeclub.github.io/mobile/android"><span class="fa-brands fa-android" aria-hidden="true"></span> Android</li>',
+            '<li><a href="https://grcodeclub.github.io/mobile/android/versions"><span class="fa-brands fa-android" aria-hidden="true"></span> Eκδόσεις</li>',
             '<li style="height: 200px;"></li>'
         ];
         addMenuLinks(menuLinks);
     }
-
-        if (document.body.id === 'android') {
-        menuLinks = [
-        '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
-        '<li><a href="https://grcodeclub.github.io/mobile">Mobile</a></li>',
-        '<li><a href="https://grcodeclub.github.io/mobile/android"><span class="fa-brands fa-android" aria-hidden="true"></span> Android</li>',
-        '<li><a href="https://grcodeclub.github.io/mobile/android/versions">Eκδόσεις</a></li>',
-        '<li style="height: 200px;"></li>'
-        ];
-        addMenuLinks(menuLinks);
-    }
-
-    
-
 };
