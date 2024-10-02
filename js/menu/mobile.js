653c2b9a54210b77d03500f848956ac1cabf4e0b
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-02 04:56:00 -0400

    Update mobile.js

--- js/menu/mobile.js
+++ js/menu/mobile.js
@@ -21,9 +21,9 @@ window.onload = function() {
         if (document.body.id === 'android') {
         var networkMenuLinks = [
         '<li><a href="https://grcodeclub.gr">Αρχική Σελίδα</a></li>',
+        '<li><a href="https://grcodeclub.github.io/mobile">Mobile</a></li>',
         '<li><a href="https://grcodeclub.github.io/mobile/android"><span class="fa-brands fa-android" aria-hidden="true"></span> Android</li>',
         '<li><a href="https://grcodeclub.github.io/mobile/android/versions">Eκδόσεις</a></li>',
-        '<li><a href="https://grcodeclub.github.io/system">Λειτουργικά Συστήματα</a></li>',
         '<li style="height: 200px;"></li>'
         ];
         addMenuLinks(menuLinks);
