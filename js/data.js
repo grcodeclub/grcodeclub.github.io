8a0b06b1a11117c8fda6be390e3451e694b901a9
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-05 12:43:15 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 // Λίστα με τους συνδέσμους προς τα αρχεία CSS
 const cssLinks = [
-    "https://grcodeclub.gr/files-page/bootstrap/v4-5-0.css",
-    "https://grcodeclub.gr/files-page/bootstrap/bootstrap-icons.css",
+    "https://grcodeclub.gr/css/bootstrap/v4-5-0.css",
+    "https://grcodeclub.gr/css/bootstrap/bootstrap-icons.css",
     "https://grcodeclub.gr/css/bootstrap/custom.css",
     "https://grcodeclub.gr/css/bootstrap/table.css",
     "https://grcodeclub.gr/css/scrollbars.css",
@@ -33,10 +33,10 @@ const cssLinks = [
  
  // Δημιουργία ενός πίνακα με τα μεγέθη που θέλετε να προσθέσετε
  const sizes = [
-   { width: 32, height: 32, url: 'https://grcodeclub.github.io/files-page/logo/32x32.png' },
-   { width: 48, height: 48, url: 'https://grcodeclub.github.io/files-page/logo/48x48.png' },
-   { width: 96, height: 96, url: 'https://grcodeclub.github.io/files-page/logo/96x96.png' },
-   { width: 144, height: 144, url: 'https://grcodeclub.github.io/files-page/logo/144x144.png' }
+   { width: 32, height: 32, url: 'https://grcodeclub.github.io/css/logo/32x32.png' },
+   { width: 48, height: 48, url: 'https://grcodeclub.github.io/css/logo/48x48.png' },
+   { width: 96, height: 96, url: 'https://grcodeclub.github.io/css/logo/96x96.png' },
+   { width: 144, height: 144, url: 'https://grcodeclub.github.io/css/logo/144x144.png' }
  ];
  
  // Προσθήκη των favicons για κάθε μέγεθος
@@ -55,7 +55,7 @@ const cssLinks = [
 const icoFaviconLink = document.createElement('link');
 icoFaviconLink.rel = 'shortcut icon';
 icoFaviconLink.type = 'image/x-icon';
-icoFaviconLink.href = 'https://grcodeclub.github.io/files-page/logo/favicon.ico';
+icoFaviconLink.href = 'https://grcodeclub.github.io/css/logo/favicon.ico';
 document.head.appendChild(icoFaviconLink);    // Προσθήκη του στοιχείου <link> στο <head> του εγγράφου
  
 ///////////////////////////////
