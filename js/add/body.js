29bfb181d381f66dcfaded288ed963d77f34361b
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-05 17:00:25 +0300

    Update body.js

--- js/add/body.js
+++ js/add/body.js
@@ -92,16 +92,32 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                         <a class="dropdown-item" href="https://grcodeclub.gr/orologia/">Γενικά</a>
                         <a class="dropdown-item" href="https://grcodeclub.gr/programming/orologia/">Προγραμματισμός</a>
                     </div>
-                       <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown8" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Kυβερνοασφάλεια</a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown8" style="max-height: 400px;">
-                    <a class="dropdown-item" href="https://grcodeclub.gr/cybersecurity">Kυβερνοασφάλεια</a>
-                    <a class="dropdown-item" href="https://grcodeclub.gr/cybersecurity/tools">Εργαλεία</a>
-                    <a class="dropdown-item" href="https://grcodeclub.gr/cybersecurity/attacks">Επιθέσεις</a>
                 </div>
             </li>
-                </div>
             </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li class="dropdown-submenu">
+                                <a class="dropdown-item dropdown-toggle" href="#">Submenu</a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Submenu Action 1</a></li>
+                                    <li><a class="dropdown-item" href="#">Submenu Action 2</a></li>
+                                    <li class="dropdown-submenu">
+                                        <a class="dropdown-item dropdown-toggle" href="#">Sub-submenu</a>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#">Sub-submenu Action 1</a></li>
+                                            <li><a class="dropdown-item" href="#">Sub-submenu Action 2</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
         </ul>
     </div>
 </nav>
