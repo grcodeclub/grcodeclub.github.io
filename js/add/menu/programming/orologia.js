b7dd748110657086129c8a72f0689cc158a1f6a3
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-05 21:28:58 +0300

    Create orologia.js

---
+++ js/add/menu/programming/orologia.js
@@ -0,0 +1,52 @@
+// Ο κώδικας παραμένει ο ίδιος για την προσθήκη του dropdown
+
+// Το HTML περιεχόμενο του dropdown
+const mymenu = `
+<div class="dropdown-container highlight">
+    <label for="page-select" class="form-label">Περιεχόμενα :</label>
+    <select id="page-select" class="form-select" onchange="redirectToPage()">
+        <option value="https://grcodeclub.gr/orologia/">Διαφορά</option>
+        <option value="https://grcodeclub.gr/network/orologia/">Δίκτυα Υπολογιστών</option>
+        <option value="https://grcodeclub.gr/programming/orologia/">Δίκτυα Υπολογιστών</option>
+        <option value="https://grcodeclub.gr/system/orologia/">Λειτουργικά Συστήματα</option>
+        <option value="https://grcodeclub.gr/linux/orologia//">Linux</option>
+        <option value="https://grcodeclub.gr/cybersecurity/orologia/">Κυβερνοασφάλεια</option>
+    </select>
+</div>
+`;
+
+// Εύρεση του στοιχείου με το id 'addHeader'
+const addHeader = document.getElementById('addHeader');
+
+// Εύρεση του επόμενου στοιχείου (next sibling) μετά το 'addHeader'
+const nextDiv = addHeader.nextElementSibling;
+
+// Προσθήκη του HTML περιεχομένου στην αρχή του επόμενου div
+if (nextDiv) {
+    nextDiv.insertAdjacentHTML('afterbegin', mymenu);
+}
+
+// Συνάρτηση για την ανακατεύθυνση στη σελίδα ανάλογα με την επιλογή
+function redirectToPage() {
+    const select = document.getElementById('page-select');
+    const selectedValue = select.value; // Παίρνουμε την τιμή του επιλεγμένου option
+    
+    if (selectedValue) {
+        window.location.href = selectedValue; // Ανακατεύθυνση στην επιλεγμένη σελίδα
+    }
+}
+
+// Ρύθμιση του επιλεγμένου option με βάση το τρέχον URL
+window.onload = function() {
+    const select = document.getElementById('page-select');
+    const currentUrl = window.location.href; // Παίρνουμε το τρέχον URL
+    
+    // Έλεγχος για να βρούμε το κατάλληλο option
+    for (let option of select.options) {
+        if (option.value === currentUrl) {
+            select.value = option.value; // Ρύθμιση του επιλεγμένου option
+            break;
+        }
+    }
+
+};
