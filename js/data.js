3fd083ad7bae99d97108e3a11108d015988589bb
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-05 21:32:45 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -151,4 +151,7 @@ function addScriptBody(src, callback) {
     const currentURL = window.location.href;  // Λειτουργία για να ελέγξει το URL και να προσθέσει id στο body
     if (currentURL.startsWith('https://grcodeclub.gr/c/')){addScriptBody('https://grcodeclub.github.io/js/add/menu/programming/c.js');}
     else if (currentURL.startsWith('https://grcodeclub.gr/python/')){addScriptBody('https://grcodeclub.github.io/js/add/menu/programming/python.js');}
+    else if (currentURL.startsWith('https://grcodeclub.gr/orologia/') || currentURL.startsWith('https://grcodeclub.gr/network/orologia/')  || currentURL.startsWith('https://grcodeclub.gr/programming/orologia/')
+             || currentURL.startsWith('https://grcodeclub.gr/system/orologia/')  || currentURL.startsWith('https://grcodeclub.gr/linux/orologia/') || currentURL.startsWith('https://grcodeclub.gr/cybersecurity/orologia/'))
+        {addScriptBody('https://grcodeclub.github.io/js/add/menu/olorogia.js');}
 });
