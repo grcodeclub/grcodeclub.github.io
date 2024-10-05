6df22d8fbdb41c25f2675f05d7894fe55bc78506
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-05 18:44:30 +0300

    Update body.js

--- js/add/body.js
+++ js/add/body.js
@@ -91,34 +91,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
                 <a class="nav-link dropdown-toggle" id="navbarDropdown3" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Επιπλέο</a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown3" style="max-height: 400px; overflow-y: auto;">
                     <li><a class="dropdown-item" href="#">Edit - Image</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="dropdown-item dropdown-toggle" id="navbarDropdown4" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Ορολογία</a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown4">
-                            <li><a class="dropdown-item" href="https://grcodeclub.gr/orologia/">Γενικά</a></li>
-                            <li><a class="dropdown-item" href="https://grcodeclub.gr/programming/orologia/">Προγραμματισμός</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li class="nav-item dropdown">
-                        <a class="dropdown-item dropdown-toggle" href="#">Submenu</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Submenu Action 1</a></li>
-                            <li><a class="dropdown-item" href="#">Submenu Action 2</a></li>
-                            <li class="nav-item dropdown">
-                                <a class="dropdown-item dropdown-toggle" href="#">Sub-submenu</a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Sub-submenu Action 1</a></li>
-                                    <li><a class="dropdown-item" href="#">Sub-submenu Action 2</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
+                    <li><a class="dropdown-item" href="https://grcodeclub.gr/orologia/">Ορολογία</a></li>
                 </ul>
             </li>
         </ul>
