88359d4b3c3256ebd6e5e0c30a20cff8f8eca277
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-06 17:03:38 +0300

    Update linux.js

--- js/add/menu/system/linux.js
+++ js/add/menu/system/linux.js
@@ -26,6 +26,7 @@ function redirectToPage() {
 }
 
 const currentURL = window.location.href; // Λειτουργία για να ελέγξει το URL και να προσθέσει id στο body
+console.log("URL:"); // Αν δεν ισχύει, εκτύπωση του τρέχοντος URL
 console.log("URL:", currentURL); // Αν δεν ισχύει, εκτύπωση του τρέχοντος URL
 if (currentURL.startsWith('https://grcodeclub.gr/linux/terminal/')) {
      console.log("Terminal Menu");
