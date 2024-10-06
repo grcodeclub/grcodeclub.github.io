638b7064872a42207bce820c5a37030c3fae48b8
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-06 23:00:32 +0300

    Update terminal.js

--- js/add/menu/system/linux/terminal.js
+++ js/add/menu/system/linux/terminal.js
@@ -99,5 +99,5 @@ window.onload = function() {
             }
             $('#select-term').select2({ placeholder: "Αναζήτηση εντολής ή σετ εντολών ", allowClear: true }); // Εφαρμογή του select2 για το select-term
         }
-    }, 5000); // 5000 ms = 5 δευτερόλεπτα
+    }, 5000); // 900 ms 
 };
