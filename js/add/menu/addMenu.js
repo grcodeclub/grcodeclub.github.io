c92841f337a2e42915eb1b343cd51dc3b60f3395
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-06 23:56:05 +0300

    Update addMenu.js

--- js/add/menu/addMenu.js
+++ js/add/menu/addMenu.js
@@ -17,7 +17,7 @@ fetch(currentURL)
             else if (currentURL.startsWith('https://grcodeclub.gr/orologia/') || currentURL.startsWith('https://grcodeclub.gr/network/orologia/') || 
                        currentURL.startsWith('https://grcodeclub.gr/programming/orologia/') ||currentURL.startsWith('https://grcodeclub.gr/system/orologia/') || 
                        currentURL.startsWith('https://grcodeclub.gr/linux/orologia/') || currentURL.startsWith('https://grcodeclub.gr/cybersecurity/orologia/')
-                       || currentURL.startsWith('https://grcodeclub.gr/windows/orologia/') ) {
+                       || currentURL.startsWith('https://grcodeclub.gr/windows/orologia') ) {
                         addScript('https://grcodeclub.github.io/js/add/menu/orologia.js'); } 
             else if (currentURL.startsWith('https://grcodeclub.gr/network/protocol/ip/')) {addScript('https://grcodeclub.github.io/js/add/menu/network/ip.js');} 
             else if (currentURL.startsWith('https://grcodeclub.gr/linux/terminal/')) {addScript('https://grcodeclub.github.io/js/add/menu/system/linux/terminal.js');}
