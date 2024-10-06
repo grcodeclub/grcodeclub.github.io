6928782a1bf9ba01e91c63aebbc1c91c6779c189
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-06 21:12:57 +0300

    Update terminal.js

--- js/add/menu/system/linux/terminal.js
+++ js/add/menu/system/linux/terminal.js
@@ -3,7 +3,7 @@
 // Το HTML περιεχόμενο του dropdown
 const termMenu = `
 <select id="term-select" class="form-select" onchange="redirectToPage()">
-      <option></option>
+      <option></option> <!-- Placeholder option -->
       <option value="https://grcodeclub.gr/linux/terminal/system/">Εντολές Συστήματος</option>
       <option value="https://grcodeclub.gr/linux/terminal/files/grep">grep</option>
       <option value="https://grcodeclub.gr/linux/terminal/system/users">Users</option>
