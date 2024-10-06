069abee314ad093891c8ee9506e15cc452e2f0bb
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-06 18:31:05 +0300

    Update addMenu.js

--- js/add/menu/addMenu.js
+++ js/add/menu/addMenu.js
@@ -1,8 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {    
-
-
 function addScript(src) {
-        var script = document.createElement('script');
+        script = document.createElement('script');
         script.src = src;
         document.body.appendChild(script);
     }
@@ -30,8 +27,3 @@ fetch(currentURL)
         // Χειρισμός σφάλματος, π.χ., σε περίπτωση δικτύου
         console.error("Σφάλμα κατά την αίτηση:", error);
     });
-
-
-
-
-});
