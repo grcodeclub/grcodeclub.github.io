6e32f0b533e237133ae9bc01cb01f39853762cdc
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-07 06:12:29 -0400

    Update website.js

--- js/add/menu/website.js
+++ js/add/menu/website.js
@@ -8,6 +8,7 @@ const mymenu = `
         <option value="https://grcodeclub.gr/">Επιλέξτε σελίδα</option>
         <option value="https://grcodeclub.gr/website/more/font-awesome">Λίστα Font Awesome</option>
         <option value="https://grcodeclub.gr/website/more/uploadsite">Κατοχύρωση, Ανέβασμα και Αναζήτηση</option>
+        <option value="https://grcodeclub.gr/website/more/uploadsite">Κωδικοί & σύμβολα χαρακτήρων HTML</option>
     </select>
 </div>
 `;
