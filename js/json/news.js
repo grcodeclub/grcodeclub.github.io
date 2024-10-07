43f77bb0389741363e99b62c380f0f3c0a4161a8
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-07 17:08:50 -0400

    Update and rename update.js to news.js

--- js/json/news.js
+++ js/json/news.js
@@ -1,6 +1,6 @@
   // Λειτουργία για την φόρτωση των δεδομένων JSON
   function loadJSON() {
-    fetch('https://grcodeclub.gr/files-page/json/update.json')
+    fetch('https://grcodeclub.gr/files-page/json/news.json')
         .then(response => response.json())
         .then(jsonData => {
             const uploadPageContainer = document.getElementById('uploadpage');
@@ -17,8 +17,8 @@
                     <div class="card-header">${item.title}</div>
                     <div class="card-body">
                         <p class="card-text">${item.description}</p>
-                        <a href="${item.link}" target="_blank" class="btn btn-dark">Προβολή σελίδας </a>
                     </div>
+                    <div class="card-footer"><small class="text-muted">${newsItem.date}</div>
                 `;
                 col.appendChild(card);
                 uploadPageContainer.appendChild(col);
