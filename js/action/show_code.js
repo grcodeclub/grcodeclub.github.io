b430b4fd4f4469a94b64b46a64188de895e71042
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-09 04:28:32 -0400

    Update show_code.js

--- js/action/show_code.js
+++ js/action/show_code.js
@@ -1,16 +1,16 @@
-const selectElement = document.getElementById("code");
+const selectElement = document.getElementById("page-select");
 const loadElement = document.getElementById("load");
 selectElement.addEventListener("change", function() {
-  update_code(selectElement.value);
+  update_page-select(selectElement.value);
 });                  
-async function update_code(selectedValue) {
+async function update_page-select(selectedValue) {
 if (selectedValue === "-") {
-  loadElement.innerHTML = '<code> </code>';
+  loadElement.innerHTML = '<page-select> </page-select>';
 } else {
   try {
    const response = await fetch(selectedValue);
    const text = await response.text();
-   loadElement.innerHTML = `<pre><code class="language-py">${escapeHtml(text)}</code></pre>`;
+   loadElement.innerHTML = `<pre><page-select class="language-py">${escapeHtml(text)}</page-select></pre>`;
    Prism.highlightAll(); // Εφαρμόστε Prism.js στο νέο περιεχόμενο
   } catch (error) {
     loadElement.innerHTML = 'Σφάλμα κατά τη φόρτωση του αρχείου.';
