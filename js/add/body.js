82b1537cf65a83ea7bdd0bad8da3aed32a5f3bb4
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-11 09:55:18 -0400

    Update body.js

--- js/add/body.js
+++ js/add/body.js
@@ -77,7 +77,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdown3" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Επιπλέο</a>
                 <ul class="dropdown-menu" aria-labelledby="navbarDropdown3" style="max-height: 400px; overflow-y: auto;">
-                    <li><a class="dropdown-item" href="https://grcodeclub.gr/info/news">Eνημερώσεις</a></li>
+                    <li><a class="dropdown-item" href="https://grcodeclub.gr/mobile">Κινητά</a></li>
                     <li><a class="dropdown-item" href="https://grcodeclub.gr/more/create/">Δημιουργία Project</a></li>
                     <li><a class="dropdown-item" href="https://grcodeclub.gr/profile/">Προφίλ Ομάδας</a></li>
                     <li><a class="dropdown-item" href="https://grcodeclub.gr/orologia/">Ορολογία</a></li>
