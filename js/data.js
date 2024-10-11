d5a121c181510321cb26aa74b88941e813acf119
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-10-11 05:06:55 -0400

    Update data.js

--- js/data.js
+++ js/data.js
@@ -112,10 +112,10 @@ document.addEventListener('DOMContentLoaded', function() {
         document.body.appendChild(script);
     }
     // Χρήση της addScriptBody για να φορτώσεις το jQuery
-    addScriptBody('https://grcodeclub.gr/js/jquery.js', function() {
+    addScriptBody('https://grcodeclub.gr/js/libraries/jquery.js', function() {
         addScriptBody('https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js', function() {
-            addScriptBody('https://grcodeclub.gr/js/bootstrap@5.3.3.js', function() {
-                addScriptBody('https://grcodeclub.gr/js/prism.js');
+            addScriptBody('https://grcodeclub.gr/js/libraries/bootstrap@5-3-3.js', function() {
+                addScriptBody('https://grcodeclub.gr/js/libraries/prism.js');
                 addScriptBody('https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js');
                 addScriptBody('https://grcodeclub.gr/js/add/body.js'); 
                 addScriptBody('https://grcodeclub.gr/js/shareURL.js');
