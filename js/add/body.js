d19c0c35815fccb75b1d1575bd00713e7a1e792d
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-12-12 14:45:27 +0200

    Update body.js

--- js/add/body.js
+++ js/add/body.js
@@ -88,7 +88,7 @@ if (addHeaderDiv) {addHeaderDiv.innerHTML = htmlContentHeader;} // Προσθή�
 
 // Περιεχόμενο footer
 const htmlContentFooter = `
-    <footer class="bg-light text-center text-lg-start mt-5">
+    <footer class="text-center text-lg-start mt-5">
         <div class="container p-4">
             <section class="mb-4">
                 <h5 class="text-uppercase text-center">Social Media</h5>
