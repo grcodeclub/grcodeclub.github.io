77c93760af4637fa12207da03f383c0cadf490d5
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-12-25 22:31:45 +0200

    Update orologia.js

--- js/add/menu/orologia.js
+++ js/add/menu/orologia.js
@@ -4,7 +4,7 @@
 const mymenu = `
 <div class="dropdown-container contentsMenu">
     <label for="page-select" class="form-label">Περιεχόμενα :</label>
-<select id="page-select" class="form-select" onchange="redirectToPage()">
+    <select id="page-select" class="form-select" onchange="redirectToPage()">
     <option value="https://grcodeclub.gr/orologia/">Διαφορά</option>
     <option value="https://grcodeclub.gr/orologia/programming">Προγραμματισμός</option>
     <option value="https://grcodeclub.gr/orologia/network">Δίκτυα Υπολογιστών</option>
@@ -12,7 +12,8 @@ const mymenu = `
     <option value="https://grcodeclub.gr/orologia/cybersecurity">Κυβερνοασφάλεια</option>
     <option value="https://grcodeclub.gr/orologia/linux">Linux</option>
     <option value="https://grcodeclub.gr/orologia/hardware">Υλικό υπολογιστών</option>
-    <option value="https://grcodeclub.gr//orologiawindows">Microsoft Windows</option>
+    <option value="https://grcodeclub.gr//orologia/windows">Microsoft Windows</option>
+    <option value="https://grcodeclub.gr//orologia/website">Ιστοσελίδας</option>
 </select>
 </div>
 `;
