0c0eb90389609cbd0c792b96d2bfcc58f6b19c29
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-12-26 01:12:03 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -144,7 +144,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 const handleScriptAddition = () => {
     const matchingScript = Object.keys(scriptMappings).find(url => currentURL.startsWith(url)); // Ελέγχει ποιο URL από τα κλειδιά του αντικειμένου `scriptMappings` ταιριάζει με το `currentURL`
-    if (matchingScript) { addScript(scriptMappings[matchingScript]); addCssFiles("https://grcodeclub.gr/css/libraries/select2/v4-0-13.css"); addCssFiles("https://grcodeclub.gr/css/libraries/select2/custom.css" }
+    if (matchingScript) { addScript(scriptMappings[matchingScript]); addCssFiles("https://grcodeclub.gr/css/libraries/select2/v4-0-13.css"); addCssFiles("https://grcodeclub.gr/css/libraries/select2/custom.css"); }
 };
 
 // Έλεγχος αν η σελίδα είναι διαθέσιμη
