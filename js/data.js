1f2357648c4aa266743ff9017c91762284dc8f5b
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-12-26 01:20:38 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -97,7 +97,7 @@ document.addEventListener('DOMContentLoaded', function() {
     addScriptBody('https://grcodeclub.gr/js/libraries/jquery.js', function() {
         addScriptBody('https://grcodeclub.gr/js/libraries/popper-v2-5-2.js', function() {
             addScriptBody('https://grcodeclub.gr/js/libraries/bootstrap@5-3-3.js', function() {
-                 if (document.querySelector('.command-line') || document.querySelector('.code_editor')) {addScriptBody('https://grcodeclub.gr/js/libraries/prism.js');}
+                 if (document.querySelector('.command-line') || document.querySelector('.code_editor')) {addScriptBody('https://grcodeclub.gr/js/libraries/prism.js');  console.log("Το Prism.js φορτώθηκε επιτυχώς.");}
                 addScriptBody('https://grcodeclub.gr/js/libraries/select2-4-0-13.js');
                 addScriptBody('https://grcodeclub.gr/js/add/body.js'); 
             });
