f62613f947aa1f4a932985a0670c890ae10176ba
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2024-12-26 18:10:14 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -9,7 +9,6 @@ const cssLinks = [
     "https://grcodeclub.gr/css/scrollbars.css",
     "https://grcodeclub.gr/css/text.css",
     "https://grcodeclub.gr/css/img.css",
-    "https://grcodeclub.gr/css/manual.css"
 ]; 
 
 const head = document.head;    // Αρχικοποίηση head
