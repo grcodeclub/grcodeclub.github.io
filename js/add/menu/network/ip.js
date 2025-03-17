b8cfdceab26c8f1dbd5ace1f3748ccf2ecb87b3c
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2025-03-17 22:39:12 +0200

    Update ip.js

--- js/add/menu/network/ip.js
+++ js/add/menu/network/ip.js
@@ -5,10 +5,10 @@ const mymenu = `
 <div class="dropdown-container contentsMenu">
     <label for="page-select" class="form-label">Περιεχόμενα :</label>
 <select id="page-select" class="form-select" onchange="redirectToPage()">
-    <option value="https://grcodeclub.gr/network/protocol/ip/">Internet Protocol</option>
-    <option value="https://grcodeclub.gr/network/protocol/ip/ipv4">Internet Protocol v4</option>
-    <option value="https://grcodeclub.gr/network/protocol/ip/ipv6">Internet Protocol v6</option>
-    <option value="https://grcodeclub.gr/network/protocol/ip/packet_IPv4">Internet Protocol v4 Μορφή Πακέτο</option>
+    <option value="https://grcodeclub.gr/network/protocol/network-address/ip/">Internet Protocol</option>
+    <option value="https://grcodeclub.gr/network/protocol/network-address/ip/ipv4">Internet Protocol v4</option>
+    <option value="https://grcodeclub.gr/network/protocol/network-address/ip/ipv6">Internet Protocol v6</option>
+    <option value="https://grcodeclub.gr/network/protocol/network-address/ip/packet_IPv4">Internet Protocol v4 Μορφή Πακέτο</option>
 </select>
 </div>
 `;
