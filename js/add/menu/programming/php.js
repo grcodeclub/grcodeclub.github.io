076e056324f366d9a25bc040ac72053e4dd3661f
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2025-03-24 12:06:45 +0200

    Update php.js

--- js/add/menu/programming/php.js
+++ js/add/menu/programming/php.js
@@ -3,10 +3,9 @@
 // Το HTML περιεχόμενο του dropdown
 const mymenu = `
 <div class="dropdown-container contentsMenu">
-    <label for="page-select" class="form-label">Περιεχόμενα :</label>
+    <label for="page-select" class="form-label">Περιεχόμενα</label>
     <select id="page-select" class="form-select" onchange="redirectToPage()">
         <option value="https://grcodeclub.gr/">Εισαγωγή</option>
-        <option value="https://github.com/grcodeclub/php/">GitHub</option>
         <option value="https://grcodeclub.gr/php/commands">Εντολές</option>
         <option value="https://grcodeclub.gr/php/files">Διαχείριση αρχείων</option>
         <option value="https://grcodeclub.gr/php/frameworks">frameworks</option>
