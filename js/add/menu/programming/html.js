c06098783218733f8c5b551ffb4a6b6ca0c728b3
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2025-03-24 12:07:31 +0200

    Update html.js

--- js/add/menu/programming/html.js
+++ js/add/menu/programming/html.js
@@ -4,7 +4,7 @@
 // Το HTML περιεχόμενο του dropdown
 const mymenu = `
 <div class="dropdown-container contentsMenu">
-    <label for="page-select" class="form-label">Περιεχόμενα :</label>
+    <label for="page-select" class="form-label">Περιεχόμενα</label>
     <select id="page-select" class="form-select" onchange="redirectToPage()">
         <option value="https://grcodeclub.gr/website/html/">Εισαγωγή</option>
         <option value="https://grcodeclub.gr/website/html/text">Kείμενο</option>
