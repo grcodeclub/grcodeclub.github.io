db3db6270251d4d2e67a431f03a8126bc11b213c
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2025-03-24 12:06:15 +0200

    Update python.js

--- js/add/menu/programming/python.js
+++ js/add/menu/programming/python.js
@@ -3,7 +3,7 @@
 // Το HTML περιεχόμενο του dropdown
 const mymenu = `
 <div class="dropdown-container contentsMenu">
-    <label for="page-select" class="form-label">Περιεχόμενα :</label>
+    <label for="page-select" class="form-label">Περιεχόμενα</label>
     <select id="page-select" class="form-select" onchange="redirectToPage()">
         <option value="https://grcodeclub.gr/python/">Εισαγωγή</option>
         <option value="https://grcodeclub.gr/python/library/">Βιβλιοθήκες</option>
