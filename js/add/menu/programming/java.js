01bcd3c4eb8a4c87639d00481c18d62f24849397
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2025-03-24 12:07:20 +0200

    Update java.js

--- js/add/menu/programming/java.js
+++ js/add/menu/programming/java.js
@@ -3,7 +3,7 @@
 // Το HTML περιεχόμενο του dropdown
 const mymenu = `
 <div class="dropdown-container contentsMenu">
-    <label for="page-select" class="form-label">Περιεχόμενα :</label>
+    <label for="page-select" class="form-label">Περιεχόμενα</label>
     <select id="page-select" class="form-select" onchange="redirectToPage()">
         <option value="https://grcodeclub.gr/java/">Εισαγωγή</option>
         <option value="https://github.com/grcodeclub/java">GitHub</option>
