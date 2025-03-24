29ee80b4bbd388f35a44b63afd716f64644a98ad
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2025-03-24 12:09:09 +0200

    Update syntomografies.js

--- js/add/menu/syntomografies.js
+++ js/add/menu/syntomografies.js
@@ -3,7 +3,7 @@
 // Το HTML περιεχόμενο του dropdown
 const mymenu = `
 <div class="dropdown-container contentsMenu">
-    <label for="page-select" class="form-label">Περιεχόμενα :</label>
+    <label for="page-select" class="form-label">Περιεχόμενα</label>
 <select id="page-select" class="form-select" onchange="redirectToPage()">
     <option value="https://grcodeclub.gr/syntomografies">Διαφορά</option>
     <option value="https://grcodeclub.gr/syntomografies/programming">Προγραμματισμός</option>
