3e4fb81852acb810c920c3c0eb319364eb53c92d
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2025-03-26 14:01:40 +0200

    Update javascript.js

--- js/add/menu/programming/javascript.js
+++ js/add/menu/programming/javascript.js
@@ -3,10 +3,13 @@
 
 // Το HTML περιεχόμενο του dropdown
 const mymenu = `
-<div class="dropdown-container contentsMenu">
-    <label for="page-select" class="form-label">Περιεχόμενα</label>
-    <select id="page-select" class="form-select" onchange="redirectToPage()">
-    <option value="https://grcodeclub.gr/javascript/">Εισαγωγή</option>
+<div class="container dropdown-container contentsMenu">
+    <div class="row g-2 align-items-center">
+        <div class="col-auto">
+            <label for="page-select" class="form-label">Περιεχόμενα</label>
+        </div>
+        <div class="col">
+        <select id="page-select" class="form-select" onchange="redirectToPage()">
     <option value="https://grcodeclub.gr/javascript/commands">Βασικές εντολές</option>
     <option value="https://grcodeclub.gr/javascript/commands/arrays">Arrays</option>
     <option value="https://grcodeclub.gr/javascript/restriction">Περιορισμοί</option>
@@ -19,9 +22,10 @@ const mymenu = `
         <option value="https://grcodeclub.gr/javascript/dom/">Βασικές Εντολές</option>
         <option value="https://grcodeclub.gr/javascript/dom/eventlisteners">EventListeners</option>
     </optgroup>
-    </select>
-</div>
-`;
+        </select>
+        </div>
+    </div>
+</div>`;
 
 // Εύρεση του στοιχείου με το id 'addHeader'
 const addHeader = document.getElementById('addHeader');
