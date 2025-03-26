d77c50a3b65cd49d2e298e210ba3829d3a08a91d
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2025-03-26 12:28:05 +0200

    Update css.js

--- js/add/menu/programming/css.js
+++ js/add/menu/programming/css.js
@@ -12,6 +12,7 @@ const mymenu = `
     <option value="https://grcodeclub.gr/website/css/media">@media</option>
     <option value="https://grcodeclub.gr/website/css/text">Κείμενο και Ιδιότητες</option>
     <option value="https://grcodeclub.gr/website/css/table">Πίνακες</option>
+    <option value="https://grcodeclub.gr/website/css/link">Link</option>
     </select>
 </div>
 `;
