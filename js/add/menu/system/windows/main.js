2a189346aa4c099de4d9364141bef99c3a449732
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2025-03-26 13:53:35 +0200

    Update main.js

--- js/add/menu/system/windows/main.js
+++ js/add/menu/system/windows/main.js
@@ -1,14 +1,20 @@
 // Το HTML περιεχόμενο του dropdown
 const mymenu = `
-<div class="dropdown-container contentsMenu">
-    <label for="page-select" class="form-label">Περιεχόμενα</label>
-    <select id="page-select" class="form-select" onchange="redirectToPage()">
+<div class="container dropdown-container contentsMenu">
+    <div class="row g-2 align-items-center">
+        <div class="col-auto">
+            <label for="page-select" class="form-label">Περιεχόμενα</label>
+        </div>
+        <div class="col">
+        <select id="page-select" class="form-select" onchange="redirectToPage()">
         <option value="https://grcodeclub.gr/windows/">Microsoft Windows</option>
         <option value="https://grcodeclub.gr/windows/command-line/">Command Line</option>
         <option value="https://grcodeclub.gr/windows/powershell">PowerShell</option>
         <option value="https://grcodeclub.gr/windows/more/run">Run Commands</option>
-        <option value="https://grcodeclub.gr/windows/more/language-id">Language ID</option>
+        <option value="https://grcodeclub.gr/windows/more/language-id">Language ID</option>            
     </select>
+        </div>
+    </div>
 </div>
 `;
 
