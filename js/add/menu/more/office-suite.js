9c2cbd943034fea0e3f23b830de717a3ecda1072
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2025-03-26 13:56:51 +0200

    Update office-suite.js

--- js/add/menu/more/office-suite.js
+++ js/add/menu/more/office-suite.js
@@ -2,13 +2,18 @@
 
 // Το HTML περιεχόμενο του dropdown
 const mymenu = `
-<div class="dropdown-container contentsMenu">
-    <label for="page-select" class="form-label">Περιεχόμενα</label>
-<select id="page-select" class="form-select" onchange="redirectToPage()">
-    <option value="https://grcodeclub.gr/more/office-suite/">Office Suite</option>
-    <option value="https://grcodeclub.gr/more/office-suite/excel">Excel</option>
-
-</select>
+<div class="container dropdown-container contentsMenu">
+    <div class="row g-2 align-items-center">
+        <div class="col-auto">
+            <label for="page-select" class="form-label">Περιεχόμενα</label>
+        </div>
+        <div class="col">
+        <select id="page-select" class="form-select" onchange="redirectToPage()">
+            <option value="https://grcodeclub.gr/more/office-suite/">Office Suite</option>
+            <option value="https://grcodeclub.gr/more/office-suite/excel">Excel</option>
+        </select>
+        </div>
+    </div>
 </div>
 `;
 
