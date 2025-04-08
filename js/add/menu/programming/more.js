0f382f6dfcc6f59b20a8349f5c3f38fcdce469d8
GrCode Club <grcodeclub@gmail.com>
2025-04-08 23:28:12 +0300

    Update File

--- js/add/menu/programming/more.js
+++ js/add/menu/programming/more.js
@@ -9,24 +9,8 @@ const mymenu = `
         </div>
         <div class="col">
         <select id="page-select" class="form-select" onchange="redirectToPage()">
-            <option value="https://grcodeclub.gr/python/">Εισαγωγή</option>
-            <option value="https://grcodeclub.gr/python/library/">Βιβλιοθήκες</option>
-            <option value="https://grcodeclub.gr/python/operators">Τελεστές</option>  
-        <optgroup label="Βασικές Εντολές">
-            <option value="https://grcodeclub.gr/python/commands/variables">Μεταβλητές</option>
-            <option value="https://grcodeclub.gr/python/commands/output_input">Είσοδος / Έξοδος</option>
-            <option value="https://grcodeclub.gr/python/commands/conditions">Δομή Ελέγχου</option>
-            <option value="https://grcodeclub.gr/python/commands/loop">Δομή Επανάληψης</option>
-            <option value="https://grcodeclub.gr/python/commands/loop">Δομή Επανάληψης</option>
-            <option value="https://grcodeclub.gr/python/commands/function">Συναρτήσεις</option>
-        </optgroup>
-            <option value="https://grcodeclub.gr/python/strings">Strings</option>
-            <option value="https://grcodeclub.gr/python/files">Αρχεία</option>
-            <option value="https://grcodeclub.gr/python/terminal">Terminal</option>
-        <optgroup label="Βασικές Εντολές">
-                <option value="https://grcodeclub.gr/python/function/">Δημιουργία Συναρτήσεων</option>
-                <option value="https://grcodeclub.gr/python/function/lambda">lambda</option>
-        </optgroup>
+            <option value="https://grcodeclub.gr/programming/">Προγραμματισμός</option>
+            <option value="https://grcodeclub.gr/programming/more/github">GitHub</option>
             </select>
         </div>
     </div>
