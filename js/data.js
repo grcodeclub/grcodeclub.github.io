d2eb62d64fd82acd49009038170d6e4fa2a19323
grcodeclub <135416047+grcodeclub@users.noreply.github.com>
2025-06-05 16:44:01 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -70,13 +70,6 @@ metaElement.setAttribute('name', 'viewport');     // Ορίζουμε την ι�
 metaElement.setAttribute('content', 'width=device-width, initial-scale=0.8');     // Ορίζουμε την ιδιότητα 'content' με την τιμή που θέλουμε
 document.head.appendChild(metaElement);     // Προσθέτουμε το στοιχείο <meta> στο <head> του HTML εγγράφου
 
-//document.addEventListener('contextmenu', event => event.preventDefault());      // Απενεργοποίηση του μενού περιβάλλοντος
-document.addEventListener('keydown', function(event) { // Αποτροπή των συντομεύσεων
-    if ((event.ctrlKey && 
-         (event.key === 'u' || event.key === 's' || event.key === 'p' || event.key === 'θ' || event.key === 'σ' || event.key === 'π' )) || 
-         (event.key === 'F12') || (event.key === 'F11')) {event.preventDefault(); alert('This keyboard shortcut is disabled on this page.');}
-});
-
 document.addEventListener('DOMContentLoaded', function() {  
 
 
