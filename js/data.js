e4c829d33d57b93853de50a955467e579a656e81
GrCode Club <grcodeclub@gmail.com>
2025-08-06 14:17:26 +0300

    no message

--- js/data.js
+++ js/data.js
@@ -154,18 +154,4 @@ fetch(currentURL)
     .catch(error => {    console.error("Σφάλμα κατά την αίτηση:", error);    });
 
 
-<<<<<<< HEAD
 });
-=======
-});
-
-
- // 1. Φορτώνουμε το script της Google AdSense δυναμικά
-    (function() {
-      var script = document.createElement('script');
-      script.async = true;
-      script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8204076348330244';
-      script.crossOrigin = 'anonymous';
-      document.head.appendChild(script);
-})();
->>>>>>> origin/main
