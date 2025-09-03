7b76b6154e6de933fb23cf68e303f1195ed6972b
GrCode Club <grcodeclub@gmail.com>
2025-09-03 18:41:40 +0300

    update file

--- files-page/js/add/menu/developer/languages/python.js
+++ files-page/js/add/menu/developer/languages/python.js
@@ -11,9 +11,6 @@ const mymenu = `
         <select id="page-select" class="form-select" onchange="redirectToPage()">
             <option value="https://grcodeclub.gr/developer/languages/python/">Εισαγωγή</option>
             <option value="https://grcodeclub.gr/developer/languages/python/library">Βιβλιοθήκες</option>
-            <option value="https://grcodeclub.gr/developer/languages/python/commands/output_input">Είσοδος / Έξοδος</option>
-            <option value="https://grcodeclub.gr/developer/languages/python/commands/conditions">Δομή Ελέγχου</option>
-            <option value="https://grcodeclub.gr/developer/languages/python/commands/loop">Δομή Επανάληψης</option>
             <option value="https://grcodeclub.gr/developer/languages/python/strings">Strings</option>
             <option value="https://grcodeclub.gr/developer/languages/python/files">Αρχεία</option>
             <option value="https://grcodeclub.gr/developer/languages/python/list">Λίστες</option>
