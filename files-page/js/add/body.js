dbc312d430b2e3878b378942029de2f8674088e3
GrCode Club <grcodeclub@gmail.com>
2025-09-08 00:39:23 +0300

    update site

--- files-page/js/add/body.js
+++ files-page/js/add/body.js
@@ -29,15 +29,9 @@ const htmlContentHeader = `
                     <li><a class="dropdown-item" href="https://grcodeclub.gr/developer/languages/java/">Java</a></li>
                     <li><a class="dropdown-item" href="https://grcodeclub.gr/developer/languages/sql">SQL</a></li>
                     <li><a class="dropdown-item" href="https://grcodeclub.gr/developer/languages/docker">Docker</a></li>
-
-                                        <li><a class="dropdown-item" href="https://grcodeclub.gr/developer/languages/c/">C</a></li>
-                    <li><a class="dropdown-item" href="https://grcodeclub.gr/developer/languages/python/">Python</a></li>
-                    <li><a class="dropdown-item" href="https://grcodeclub.gr/developer/languages/cpp/">C++</a></li>
-                    <li><a class="dropdown-item" href="https://grcodeclub.gr/developer/languages/java/">Java</a></li>
-                    <li><a class="dropdown-item" href="https://grcodeclub.gr/developer/languages/sql">SQL</a></li>
-                    <li><a class="dropdown-item" href="https://grcodeclub.gr/developer/languages/docker">Docker</a></li>
-
-                    
+                    <li><a class="dropdown-item" href="https://grcodeclub.gr/developer/languages/latex">LaTeX</a></li>
+                    <li><a class="dropdown-item" href="https://grcodeclub.gr/developer/languages/matlab">Matlab</a></li>
+                    <li><a class="dropdown-item" href="https://grcodeclub.gr/developer/languages/markdown">Markdown</a></li>
                 </ul>
             </li>
             
