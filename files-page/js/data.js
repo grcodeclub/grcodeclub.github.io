7b84949cd1097bd68e5cf373c55147cf44248baa
GrCode Club <grcodeclub@gmail.com>
2025-09-10 00:55:18 +0300

    update site

--- files-page/js/data.js
+++ files-page/js/data.js
@@ -114,12 +114,10 @@ loadScript('https://grcodeclub.github.io/files-page/js/libraries/jquery.js')
     const scriptMappings = {
     'https://grcodeclub.gr/web-developer/javascript/': 'https://grcodeclub.github.io/files-page/js/add/menu/developer/web/javascript.js',
     'https://grcodeclub.gr/web-developer/css/': 'https://grcodeclub.github.io/files-page/js/add/menu/developer/web/css.js',
-    'https://grcodeclub.gr/web-developer/nodejs/': 'https://grcodeclub.github.io/files-page/js/add/menu/developer/web/nodejs.js',
     'https://grcodeclub.gr/web-developer/cms/wordpress': 'https://grcodeclub.github.io/files-page/js/add/menu/developer/web/cms/wordpress.js',
     'https://grcodeclub.gr/web-developer/build/': 'https://grcodeclub.github.io/files-page/js/add/menu/website.js',
-    'https://grcodeclub.gr/operating-systems/microsoft-windows/': 'https://grcodeclub.github.io/files-page/js/add/menu/system/windows.js',
-    'https://grcodeclub.gr/operating-systems/linux/terminal/': 'https://grcodeclub.github.io/files-page/js/add/menu/system/linux/terminal.js',
-    'https://grcodeclub.gr/operating-systems/linux/': 'https://grcodeclub.github.io/files-page/js/add/menu/system/linux/menu.js',
+    'https://grcodeclub.gr/operating-systems/microsoft-windows/': 'https://grcodeclub.github.io/files-page/js/add/menu/windows.js',
+    'https://grcodeclub.gr/operating-systems/linux/': 'https://grcodeclub.github.io/files-page/js/add/menu/linux.js',
     'https://grcodeclub.gr/syntomografies/': 'https://grcodeclub.github.io/files-page/js/add/menu/syntomografies.js',
     'https://grcodeclub.gr/orologia/': 'https://grcodeclub.github.io/files-page/js/add/menu/orologia.js'
 };
