84726fb58bc343a423f6b53d851ece755635cd38
GrCode Club <grcodeclub@gmail.com>
2025-09-21 00:51:40 +0300

    update site

--- files-page/js/add/menu/website.js
+++ files-page/js/add/menu/website.js
@@ -9,12 +9,12 @@ const mymenu = `
         </div>
         <div class="col">
         <select id="page-select" class="form-select" onchange="redirectToPage()">
-        <option value="https://grcodeclub.gr/web-developer/">Εισαγωγή</option>
+        <option value="https://grcodeclub.gr/web-developer/build/">Κατασκευή Ιστοσελίδων</option>
         <option value="https://grcodeclub.gr/web-developer/build/uploadsite">Κατοχύρωση Website</option>
         <option value="https://grcodeclub.gr/web-developer/build/seo">SEO</option>
         <option value="https://grcodeclub.gr/web-developer/build/font-awesome">Λίστα Font Awesome</option>
-        <option value="https://grcodeclub.gr/web-developer/build/input-symbols">Κωδικοί & σύμβολα χαρακτήρων HTML</option>
-        <option value="https://grcodeclub.gr/web-developer/build/libraries/boostrap">Class Bootstrap (5.3.3)</option>
+        <option value="https://grcodeclub.gr/web-developer/build/input-symbols">Κωδικοί & Σύμβολα Χαρακτήρων HTML</option>
+        <option value="https://grcodeclub.gr/web-developer/build/libraries/boostrap">Class Bootstrap</option>
             </select>
         </div>
     </div>
